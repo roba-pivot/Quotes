2bf6c0e972c62d08b34126c76f29a0a7a017a892
komen225 <komen.robert19@gmail.com>
2019-08-18 15:14:46 +0300

    Add on quote form .ts rewrite.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  newQuotes = new Quotes (0,"","","",new Date(),0,0)
+
+  @Output () sendData = new EventEmitter<Quotes>();
+
+  sendForm (){
+    this.sendData.emit(this.newQuotes);
+  }
 
   constructor() { }
 
